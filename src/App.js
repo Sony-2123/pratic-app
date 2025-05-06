e1dc363ce3266cb2dfc1db38690dd17ef86419f8
Sony-2123 <sonyreddy7406@gmail.com>
2025-05-06 16:14:45 +0530

    jsx,rules,exports,fragments

--- src/App.js
+++ src/App.js
@@ -2,15 +2,56 @@ import Button from './components/button';
 import Table from './components/table';
  import List from './components/list';
  import Form from './components/form';
+ import  Heading1,{Heading2,Heading3,Heading4,Heading5} from './components/headings/headings';
 function App() {
-  return (
-    <div>
-      <Button />
-      <Table/> 
-      <List/>
-      <Form/>
-    </div>
-  );
-}
+  // const employee={
+  //   name:"sony",
+  //   secondName:"pandikunta" 
+  //  }
+  //  const {name,secondName}=employee
+  // return (
+  //   <div className="hello">
+  //     <h1>hi what is your name {employee.name}</h1>
+  //     <h1>hi what is your secondname {employee.secondName}</h1>
+  //     <Button />
+  //     <Table/> 
+  //     <List/>
+  //     <Form/> 
+  //   </div>
+//   );
+// }
 
+
+// const isSubscried=false
+// if(isSubscried){
+//   // return<h1>Thanks for Subscried</h1><Table/>
+// }
+//     else {
+//       return<h1>please Subscribe</h1>
+//     }
+//   }
+
+
+// return (
+//   <div>
+//     isSubscried
+//     ?
+//     <List/>
+//     :
+// <h1>please Subscribe</h1>
+//   </div>
+// )
+// }
+
+const isSubscried=false
+return(
+  <div>
+    <Heading1/>
+    <Heading2/>
+    <Heading3/>
+    <Heading4/>
+    <Heading5/>
+  </div>
+)
+}
 export default App
---
+++ src/components/headings/headings.js
@@ -0,0 +1,39 @@
+export const Heading1 = ()=>{
+    return(
+        <div>
+            <h1>H1 Heading</h1>
+        </div>
+    )
+}
+
+export const Heading2 = ()=>{
+    return(
+        <div>
+            <h1>H1 Heading</h1>
+        </div>
+    )
+}
+
+export const Heading3 = ()=>{
+    return(
+        <div>
+            <h1>H1 Heading</h1>
+        </div>
+    )
+}
+
+export const Heading4 = ()=>{
+    return(
+        <div>
+            <h1>H1 Heading</h1>
+        </div>
+    )
+}
+export const Heading5 = ()=>{
+    return(
+        <div>
+            <h1>H1 Heading</h1>
+        </div>
+    )
+}
+
